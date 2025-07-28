f6eb3756a73d9839691ede5dec5614a2fa6be799
a <b>
2025-07-28 22:14:36 +0200

    neotree,reihenzahlen

--- init.lua
+++ init.lua
@@ -266,6 +266,25 @@ require('lazy').setup({
   },
   { 'iamcco/markdown-preview.nvim', lazy = false },
   --
+  'sitiom/nvim-numbertoggle',
+  'MunifTanjim/nui.nvim',
+  'nvim-lua/plenary.nvim',
+  --
+  --
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons', -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  },
+
+  --
+  --
+  --
   --
   --
   --
