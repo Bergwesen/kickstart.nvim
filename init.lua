554a44cfc16a9ad628f43f3c33374d52422b38fa
k <kevv2@protonmail.com>
2025-09-30 19:47:27 +0200

    theme geaendert

--- init.lua
+++ init.lua
@@ -253,7 +253,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
-  -- NOTE: Plugins can also be added by using a table,
+  -- NOTE: Plugins can also be added by using a table,/
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
   --
@@ -291,6 +291,9 @@ require('lazy').setup({
       },
     },
     lazy = false, -- neo-tree will lazily load itself
+  },
+  {
+    'barrett-ruth/live-server.nvim',
   },
 
   --
@@ -940,9 +943,11 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
+
+  'sainnhe/gruvbox-material',
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
