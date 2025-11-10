36e8dca08ad8b77f78a5b3f96152ea08766b9640
a <kevv2@protonmail.com>
2025-11-10 03:27:15 +0100

    latex

--- init.lua
+++ init.lua
@@ -293,6 +293,15 @@ require('lazy').setup({
     lazy = false, -- neo-tree will lazily load itself
   },
   {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+    end,
+  },
+  {
     'barrett-ruth/live-server.nvim',
   },
 
